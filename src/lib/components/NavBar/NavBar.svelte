38fa99e9584f4abfc51b0c1b2127e7e18615b374
Idrking <idrkings@gmail.com>
2022-09-05 16:32:59 -0700

    Moved lib folder to src. initial work on nav

---
+++ src/lib/components/NavBar/NavBar.svelte
@@ -0,0 +1,42 @@
+<nav>
+	<menu>
+		<li>BUTTON</li>
+		<li>BUTTON</li>
+		<li>BUTTON</li>
+	</menu>
+	<a href="/">
+		<img src="/logoMain.png" alt="The Raptor Rescue Society Logo" />
+	</a>
+</nav>
+
+<style>
+	nav {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		display: flex;
+	}
+
+	a {
+		flex: 1;
+		border: 2px solid blue;
+		display: flex;
+		justify-content: center;
+		height: 20rem;
+
+		background-color: var(--white-main);
+	}
+
+	img {
+		aspect-ratio: 93/50;
+		height: 100%;
+		border: 2px solid deeppink;
+	}
+
+	menu {
+		flex: 3;
+		border: 2px solid red;
+		height: 5rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
-	import Header from '../../lib/components/Header.svelte';
+	import Header from '../lib/components/Header.svelte';
+	import NavBar from '../lib/components/NavBar/NavBar.svelte';
 </script>
 
+<NavBar />
 <Header />
 <p>this appears on pages</p>
 <slot />
